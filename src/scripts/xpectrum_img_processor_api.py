c38eb26cafa8ce74f96b7689a286479fc203c6a3
Kim Clarence Penaflor <kimc.penaflor@gmail.com>
2021-02-21 18:00:04 +0800

    add s3 file handler in api

--- src/scripts/xpectrum_img_processor_api.py
+++ src/scripts/xpectrum_img_processor_api.py
@@ -1,19 +1,44 @@
 
+import io
+import os
 import json
-import src.aws.s3 import S3
+from src.aws.s3 import S3
+from src.img_processor import transform
+
+global BASE_WIDTH
+BASE_WIDTH = 450
 
 def handler(event, context):
+    global BASE_WIDTH
+
     """
     event:
-        s3DirPrefix: Output Directory Image Prefix (folder) 
-        base_width: Thumbnail base width
+        cType: image content/mime type
+        s3ImgDir: s3 image input directory
+        baseWidth: Thumbnail base width
     """
+
+    BASE_WIDTH = event['baseWidth']
+
+    s3 = S3(
+        bucketName = os.environ['bucketname']
+    )
+    image_raw = s3.load_resource_bytes(key=event['s3ImgDir'])
+    img_input = io.BytesIO(image_raw)
+    img_output = io.BytesIO()
+
+
+    img_transformed = transform.resize(BASE_WIDTH, img_input)
+    img_transformed.save(img_output)
+
+    s3_output_dir = 'thumbnail_'+event['s3ImgDir'].split('/')[-1]
+    s3.save_resource(s3_output_dir, img_output.getvalue(), event['cType'] )
+    
     return {
         'statusCode': 200,
         'body': json.dumps({
-            's3FileUrl': ''
+            'msg': 'OK'
         })
     }
-
 
 
