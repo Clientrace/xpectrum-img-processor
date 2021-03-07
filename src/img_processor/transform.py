6fcc27171c4b69c3042d77c40ebbc1eb08578939
Kim Clarence Penaflor <kimc.penaflor@gmail.com>
2021-03-07 23:16:34 +0800

    add image convert to RGB (png > jpg) in resize

--- src/img_processor/transform.py
+++ src/img_processor/transform.py
@@ -4,13 +4,14 @@ from PIL import Image
 
 def resize(base_width, img_blob):
     img = Image.open(img_blob)
+    img.convert('RGB')
     img_width, img_height = img.size
 
     ratio = (base_width/float(img_width))
     height = int((float(img_height) * float(ratio)))
     img = img.resize((base_width, height), Image.ANTIALIAS)
+    img = img.convert('RGB')
     return img
-
 
 
 
